f4aed1784304f5cbcc7aed64202064d3c18c7cf2
freitzzz <1160907@isep.ipp.pt>
2023-02-26 10:58:23 +0000

    send free desktop notification if --allow-notifications argument is present

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { config as dotenv } from 'dotenv';
 import { Client } from 'tmi.js';
+import { exec } from 'child_process';
 
 dotenv();
+
+const allowNotifications = process.argv.includes('--allow-notifications');
 
 const channelName = process.env.twitch_channel_name;
 
@@ -12,5 +15,29 @@ const client = new Client({
 client.connect();
 
 client.on('message', (_, tags, message, __) => {
-    console.log(`${tags['display-name']}: ${message}`);
+    const displayName = tags['display-name'];
+
+    console.info(`${displayName}: ${message}`);
+
+    if (allowNotifications) {
+        freeDesktopNotification(sanitize(displayName), sanitize(message));
+    }
 });
+
+function freeDesktopNotification(displayName, message) {
+    exec(
+        `gdbus call --session \
+        --dest=org.freedesktop.Notifications \
+        --object-path=/org/freedesktop/Notifications \
+        --method=org.freedesktop.Notifications.Notify \
+        "Twitch Chat" "0" "0" "from: ${displayName}" "${message}" \
+        [] "{}" 3000
+        `
+    );
+
+    exec('paplay "/usr/share/sounds/freedesktop/stereo/message-new-instant.oga"');
+}
+
+function sanitize(str) {
+    return str.replace(/[^A-Za-z0-9\?\!\;\,\-\_\ \:\=]/g, '').substring(0, 70);
+}
